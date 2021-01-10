55b3a67fb9564a9b128412fdd890144f10252554
Karwan-018 <karwannazm8@gmail.com>
2021-01-10 22:23:48 +0300

    commit done

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Info
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("name ");
+            string name = Console.In.ReadLine();
+            Console.WriteLine("age ");
+            int age = Convert.ToInt32(Console.In.ReadLine());
+            Console.WriteLine("job ");
+            string job = Console.In.ReadLine();
+            Console.WriteLine("name:  " + name + "\njob: "+job+"\nage: "+age+" years old");
+        }
+    }
+}
